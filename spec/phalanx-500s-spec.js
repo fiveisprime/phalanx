126b0b586723a4d1b8a5c5f9605d1042ec4e9d17
Matt Hernandez <matt@modulus.io>
2013-10-12 13:24:24 -0400

    Normalize the error message.

--- spec/phalanx-500s-spec.js
+++ spec/phalanx-500s-spec.js
@@ -10,7 +10,7 @@ describe('500 status codes', function() {
 
     it('should send a 500 for internal server error', function() {
       spyOn(res, 'send');
-      phalanx.internalServerError('Internal error.').send(res);
+      phalanx.internalServerError('Error').send(res);
 
       expect(res.send).wasCalled();
       expect(res.statusCode).toEqual(500);
@@ -18,7 +18,7 @@ describe('500 status codes', function() {
 
     it('should send a 501 for not implemented', function() {
       spyOn(res, 'send');
-      phalanx.notImplemented('Internal error.').send(res);
+      phalanx.notImplemented('Error').send(res);
 
       expect(res.send).wasCalled();
       expect(res.statusCode).toEqual(501);
@@ -26,7 +26,7 @@ describe('500 status codes', function() {
 
     it('should send a 502 for bad gateway', function() {
       spyOn(res, 'send');
-      phalanx.badGateway('Internal error.').send(res);
+      phalanx.badGateway('Error').send(res);
 
       expect(res.send).wasCalled();
       expect(res.statusCode).toEqual(502);
@@ -34,7 +34,7 @@ describe('500 status codes', function() {
 
     it('should send a 503 for service unavailable', function() {
       spyOn(res, 'send');
-      phalanx.serviceUnavailable('Internal error.').send(res);
+      phalanx.serviceUnavailable('Error').send(res);
 
       expect(res.send).wasCalled();
       expect(res.statusCode).toEqual(503);
@@ -42,7 +42,7 @@ describe('500 status codes', function() {
 
     it('should send a 504 for gateway timeout', function() {
       spyOn(res, 'send');
-      phalanx.gatewayTimeout('Internal error.').send(res);
+      phalanx.gatewayTimeout('Error').send(res);
 
       expect(res.send).wasCalled();
       expect(res.statusCode).toEqual(504);
@@ -54,7 +54,7 @@ describe('500 status codes', function() {
 
     it('should send a 500 and render a view for internal server error', function() {
       spyOn(res, 'render');
-      phalanx.internalServerError('Internal error.').render(res, 'test');
+      phalanx.internalServerError('Error').render(res, 'test');
 
       expect(res.render).wasCalled();
       expect(res.statusCode).toEqual(500);
@@ -62,7 +62,7 @@ describe('500 status codes', function() {
 
     it('should send a 501 and render a view for not implemented', function() {
       spyOn(res, 'render');
-      phalanx.notImplemented('Internal error.').render(res, 'test');
+      phalanx.notImplemented('Error').render(res, 'test');
 
       expect(res.render).wasCalled();
       expect(res.statusCode).toEqual(501);
@@ -70,7 +70,7 @@ describe('500 status codes', function() {
 
     it('should send a 502 and render a view for bad gateway', function() {
       spyOn(res, 'render');
-      phalanx.badGateway('Internal error.').render(res, 'test');
+      phalanx.badGateway('Error').render(res, 'test');
 
       expect(res.render).wasCalled();
       expect(res.statusCode).toEqual(502);
@@ -78,7 +78,7 @@ describe('500 status codes', function() {
 
     it('should send a 503 and render a view for service unavailable', function() {
       spyOn(res, 'render');
-      phalanx.serviceUnavailable('Internal error.').render(res, 'test');
+      phalanx.serviceUnavailable('Error').render(res, 'test');
 
       expect(res.render).wasCalled();
       expect(res.statusCode).toEqual(503);
@@ -86,7 +86,7 @@ describe('500 status codes', function() {
 
     it('should send a 504 and render a view for gateway timeout', function() {
       spyOn(res, 'render');
-      phalanx.gatewayTimeout('Internal error.').render(res, 'test');
+      phalanx.gatewayTimeout('Error').render(res, 'test');
 
       expect(res.render).wasCalled();
       expect(res.statusCode).toEqual(504);
