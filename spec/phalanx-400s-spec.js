bd7e53b1f50f00968e516c10204a06a6a5de7bce
Matt Hernandez <matt@modulus.io>
2014-03-27 11:40:31 -0400

    Cleaned up tests and make.
    The shell scripts set the env so there is no need to explicitly run with
    node.

--- spec/phalanx-400s-spec.js
+++ spec/phalanx-400s-spec.js
@@ -18,70 +18,70 @@ describe('400 status codes', function() {
     it('should send a 400 for bad request', function() {
       phalanx.badRequest('Error').send(res);
 
-      res.send.called.should.equal(true);
+      res.send.called.should.be.true;
       res.statusCode.should.equal(400);
     });
 
     it('should send a 401 for unauthorized', function() {
       phalanx.unauthorized('Error').send(res);
 
-      res.send.called.should.equal(true);
+      res.send.called.should.be.true;
       res.statusCode.should.equal(401);
     });
 
     it('should send a 403 for forbidden', function() {
       phalanx.forbidden('Error').send(res);
 
-      res.send.called.should.equal(true);
+      res.send.called.should.be.true;
       res.statusCode.should.equal(403);
     });
 
     it('should send a 404 for not found', function() {
       phalanx.notFound('Error').send(res);
 
-      res.send.called.should.equal(true);
+      res.send.called.should.be.true;
       res.statusCode.should.equal(404);
     });
 
     it('should send a 405 for method not allowed', function() {
       phalanx.methodNotAllowed('Error').send(res);
 
-      res.send.called.should.equal(true);
+      res.send.called.should.be.true;
       res.statusCode.should.equal(405);
     });
 
     it('should send a 406 for not acceptable', function() {
       phalanx.notAcceptable('Error').send(res);
 
-      res.send.called.should.equal(true);
+      res.send.called.should.be.true;
       res.statusCode.should.equal(406);
     });
 
     it('should send a 407 for proxy authentication required', function() {
       phalanx.proxyAuthenticationRequired('Error').send(res);
 
-      res.send.called.should.equal(true);
+      res.send.called.should.be.true;
       res.statusCode.should.equal(407);
     });
 
     it('should send a 408 for request timeout', function() {
       phalanx.requestTimeout('Error').send(res);
 
-      res.send.called.should.equal(true);
+      res.send.called.should.be.true;
       res.statusCode.should.equal(408);
     });
 
     it('should send a 409 for conflict', function() {
       phalanx.conflict('Error').send(res);
 
-      res.send.called.should.equal(true);
+      res.send.called.should.be.true;
       res.statusCode.should.equal(409);
     });
 
     it('should send a 410 for gone', function() {
       phalanx.gone('Error').send(res);
 
-      res.send.called.should.equal(true);
+      res.send.called.should.be.true;
       res.statusCode.should.equal(410);
     });
 
@@ -97,80 +97,80 @@ describe('400 status codes', function() {
     it('should send a 400 and render a view for bad request', function() {
       phalanx.badRequest('Error').render(res, 'test');
 
-      res.render.called.should.equal(true);
-      res.render.calledWith('test', { error: 'Error' }).should.equal(true);
+      res.render.called.should.be.true;
+      res.render.calledWith('test', { error: 'Error' }).should.be.true;
       res.statusCode.should.equal(400);
     });
 
     it('should send a 401 and render a view for unauthorized', function() {
       phalanx.unauthorized('Error').render(res, 'test');
 
-      res.render.called.should.equal(true);
-      res.render.calledWith('test', { error: 'Error' }).should.equal(true);
+      res.render.called.should.be.true;
+      res.render.calledWith('test', { error: 'Error' }).should.be.true;
       res.statusCode.should.equal(401);
     });
 
     it('should send a 403 and render a view for forbidden', function() {
       phalanx.forbidden('Error').render(res, 'test');
 
-      res.render.called.should.equal(true);
-      res.render.calledWith('test', { error: 'Error' }).should.equal(true);
+      res.render.called.should.be.true;
+      res.render.calledWith('test', { error: 'Error' }).should.be.true;
       res.statusCode.should.equal(403);
     });
 
     it('should send a 404 and render a view for not found', function() {
       phalanx.notFound('Error').render(res, 'test');
 
-      res.render.called.should.equal(true);
-      res.render.calledWith('test', { error: 'Error' }).should.equal(true);
+      res.render.called.should.be.true;
+      res.render.calledWith('test', { error: 'Error' }).should.be.true;
       res.statusCode.should.equal(404);
     });
 
     it('should send a 405 and render a view for method not allowed', function() {
       phalanx.methodNotAllowed('Error').render(res, 'test');
 
-      res.render.called.should.equal(true);
-      res.render.calledWith('test', { error: 'Error' }).should.equal(true);
+      res.render.called.should.be.true;
+      res.render.calledWith('test', { error: 'Error' }).should.be.true;
       res.statusCode.should.equal(405);
     });
 
     it('should send a 406 and render a view for not acceptable', function() {
       phalanx.notAcceptable('Error').render(res, 'test');
 
-      res.render.called.should.equal(true);
-      res.render.calledWith('test', { error: 'Error' }).should.equal(true);
+      res.render.called.should.be.true;
+      res.render.calledWith('test', { error: 'Error' }).should.be.true;
       res.statusCode.should.equal(406);
     });
 
     it('should send a 407 and render a view for proxy authentication required', function() {
       phalanx.proxyAuthenticationRequired('Error').render(res, 'test');
 
-      res.render.called.should.equal(true);
-      res.render.calledWith('test', { error: 'Error' }).should.equal(true);
+      res.render.called.should.be.true;
+      res.render.calledWith('test', { error: 'Error' }).should.be.true;
       res.statusCode.should.equal(407);
     });
 
     it('should send a 408 and render a view for request timeout', function() {
       phalanx.requestTimeout('Error').render(res, 'test');
 
-      res.render.called.should.equal(true);
-      res.render.calledWith('test', { error: 'Error' }).should.equal(true);
+      res.render.called.should.be.true;
+      res.render.calledWith('test', { error: 'Error' }).should.be.true;
       res.statusCode.should.equal(408);
     });
 
     it('should send a 409 and render a view for conflict', function() {
       phalanx.conflict('Error').render(res, 'test');
 
-      res.render.called.should.equal(true);
-      res.render.calledWith('test', { error: 'Error' }).should.equal(true);
+      res.render.called.should.be.true;
+      res.render.calledWith('test', { error: 'Error' }).should.be.true;
       res.statusCode.should.equal(409);
     });
 
     it('should send a 410 and render a view for gone', function() {
       phalanx.gone('Error').render(res, 'test');
 
-      res.render.called.should.equal(true);
-      res.render.calledWith('test', { error: 'Error' }).should.equal(true);
+      res.render.called.should.be.true;
+      res.render.calledWith('test', { error: 'Error' }).should.be.true;
       res.statusCode.should.equal(410);
     });
 
@@ -186,70 +186,70 @@ describe('400 status codes', function() {
     it('should send json a 400 for bad request', function() {
       phalanx.badRequest('Error').json(res);
 
-      res.json.called.should.equal(true);
+      res.json.called.should.be.true;
       res.statusCode.should.equal(400);
     });
 
     it('should send json a 401 for unauthorized', function() {
       phalanx.unauthorized('Error').json(res);
 
-      res.json.called.should.equal(true);
+      res.json.called.should.be.true;
       res.statusCode.should.equal(401);
     });
 
     it('should send json a 403 for forbidden', function() {
       phalanx.forbidden('Error').json(res);
 
-      res.json.called.should.equal(true);
+      res.json.called.should.be.true;
       res.statusCode.should.equal(403);
     });
 
     it('should send json a 404 for not found', function() {
       phalanx.notFound('Error').json(res);
 
-      res.json.called.should.equal(true);
+      res.json.called.should.be.true;
       res.statusCode.should.equal(404);
     });
 
     it('should send json a 405 for method not allowed', function() {
       phalanx.methodNotAllowed('Error').json(res);
 
-      res.json.called.should.equal(true);
+      res.json.called.should.be.true;
       res.statusCode.should.equal(405);
     });
 
     it('should send json a 406 for not acceptable', function() {
       phalanx.notAcceptable('Error').json(res);
 
-      res.json.called.should.equal(true);
+      res.json.called.should.be.true;
       res.statusCode.should.equal(406);
     });
 
     it('should send a json 407 for proxy authentication required', function() {
       phalanx.proxyAuthenticationRequired('Error').json(res);
 
-      res.json.called.should.equal(true);
+      res.json.called.should.be.true;
       res.statusCode.should.equal(407);
     });
 
     it('should send a json 408 for request timeout', function() {
       phalanx.requestTimeout('Error').json(res);
 
-      res.json.called.should.equal(true);
+      res.json.called.should.be.true;
       res.statusCode.should.equal(408);
     });
 
     it('should send a json 409 for conflict', function() {
       phalanx.conflict('Error').json(res);
 
-      res.json.called.should.equal(true);
+      res.json.called.should.be.true;
       res.statusCode.should.equal(409);
     });
 
     it('should send a json 410 for gone', function() {
       phalanx.gone('Error').json(res);
 
-      res.json.called.should.equal(true);
+      res.json.called.should.be.true;
       res.statusCode.should.equal(410);
     });
 
--- spec/phalanx-500s-spec.js
+++ spec/phalanx-500s-spec.js
@@ -18,35 +18,35 @@ describe('500 status codes', function() {
     it('should send a 500 for internal server error', function() {
       phalanx.internalServerError('Error').send(res);
 
-      res.send.called.should.equal(true);
+      res.send.called.should.be.true;
       res.statusCode.should.equal(500);
     });
 
     it('should send a 501 for not implemented', function() {
       phalanx.notImplemented('Error').send(res);
 
-      res.send.called.should.equal(true);
+      res.send.called.should.be.true;
       res.statusCode.should.equal(501);
     });
 
     it('should send a 502 for bad gateway', function() {
       phalanx.badGateway('Error').send(res);
 
-      res.send.called.should.equal(true);
+      res.send.called.should.be.true;
       res.statusCode.should.equal(502);
     });
 
     it('should send a 503 for service unavailable', function() {
       phalanx.serviceUnavailable('Error').send(res);
 
-      res.send.called.should.equal(true);
+      res.send.called.should.be.true;
       res.statusCode.should.equal(503);
     });
 
     it('should send a 504 for gateway timeout', function() {
       phalanx.gatewayTimeout('Error').send(res);
 
-      res.send.called.should.equal(true);
+      res.send.called.should.be.true;
       res.statusCode.should.equal(504);
     });
 
@@ -62,40 +62,40 @@ describe('500 status codes', function() {
     it('should send a 500 and render a view for internal server error', function() {
       phalanx.internalServerError('Error').render(res, 'test');
 
-      res.render.called.should.equal(true);
-      res.render.calledWith('test', { error: 'Error' }).should.equal(true);
+      res.render.called.should.be.true;
+      res.render.calledWith('test', { error: 'Error' }).should.be.true;
       res.statusCode.should.equal(500);
     });
 
     it('should send a 501 and render a view for not implemented', function() {
       phalanx.notImplemented('Error').render(res, 'test');
 
-      res.render.called.should.equal(true);
-      res.render.calledWith('test', { error: 'Error' }).should.equal(true);
+      res.render.called.should.be.true;
+      res.render.calledWith('test', { error: 'Error' }).should.be.true;
       res.statusCode.should.equal(501);
     });
 
     it('should send a 502 and render a view for bad gateway', function() {
       phalanx.badGateway('Error').render(res, 'test');
 
-      res.render.called.should.equal(true);
-      res.render.calledWith('test', { error: 'Error' }).should.equal(true);
+      res.render.called.should.be.true;
+      res.render.calledWith('test', { error: 'Error' }).should.be.true;
       res.statusCode.should.equal(502);
     });
 
     it('should send a 503 and render a view for service unavailable', function() {
       phalanx.serviceUnavailable('Error').render(res, 'test');
 
-      res.render.called.should.equal(true);
-      res.render.calledWith('test', { error: 'Error' }).should.equal(true);
+      res.render.called.should.be.true;
+      res.render.calledWith('test', { error: 'Error' }).should.be.true;
       res.statusCode.should.equal(503);
     });
 
     it('should send a 504 and render a view for gateway timeout', function() {
       phalanx.gatewayTimeout('Error').render(res, 'test');
 
-      res.render.called.should.equal(true);
-      res.render.calledWith('test', { error: 'Error' }).should.equal(true);
+      res.render.called.should.be.true;
+      res.render.calledWith('test', { error: 'Error' }).should.be.true;
       res.statusCode.should.equal(504);
     });
 
@@ -111,35 +111,35 @@ describe('500 status codes', function() {
     it('should send a json 500 for internal server error', function() {
       phalanx.internalServerError('Error').json(res);
 
-      res.json.called.should.equal(true);
+      res.json.called.should.be.true;
       res.statusCode.should.equal(500);
     });
 
     it('should send a json 501 for not implemented', function() {
       phalanx.notImplemented('Error').json(res);
 
-      res.json.called.should.equal(true);
+      res.json.called.should.be.true;
       res.statusCode.should.equal(501);
     });
 
     it('should send a json 502 for bad gateway', function() {
       phalanx.badGateway('Error').json(res);
 
-      res.json.called.should.equal(true);
+      res.json.called.should.be.true;
       res.statusCode.should.equal(502);
     });
 
     it('should send a json 503 for service unavailable', function() {
       phalanx.serviceUnavailable('Error').json(res);
 
-      res.json.called.should.equal(true);
+      res.json.called.should.be.true;
       res.statusCode.should.equal(503);
     });
 
     it('should send a json 504 for gateway timeout', function() {
       phalanx.gatewayTimeout('Error').json(res);
 
-      res.json.called.should.equal(true);
+      res.json.called.should.be.true;
       res.statusCode.should.equal(504);
     });
 
